86035148db85b2f548eb89da258a7cadc8bb814f
Isabela Louise <isabelalouise.cs@gmail.com>
2025-11-06 22:17:53 -0300

    add interface

---
+++ TesteSwing.java
@@ -0,0 +1,9 @@
+import javax.swing.JFrame;
+
+public class TesteSwing {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Teste Swing");
+        frame.setSize(200, 100);
+        frame.setVisible(true);
+    }
+}
