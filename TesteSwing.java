3f3c1bc41832f810a76dc8b5a0a4e84a527c2c9c
Isabela Louise <isabelalouise.cs@gmail.com>
2025-11-06 22:28:41 -0300

    testando javaswing

--- TesteSwing.java
+++
@@ -1,9 +0,0 @@
-import javax.swing.JFrame;
-
-public class TesteSwing {
-    public static void main(String[] args) {
-        JFrame frame = new JFrame("Teste Swing");
-        frame.setSize(200, 100);
-        frame.setVisible(true);
-    }
-}
